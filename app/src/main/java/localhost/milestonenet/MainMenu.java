d14ea805d6cedbecdfd342551d93a2ee7e4ccefc
zacharyfeininger <zaf83063@uga.edu>
2024-12-27 16:29:39 -0500

    buttons added, some functionality

--- app/src/main/java/localhost/milestonenet/MainMenu.java
+++ app/src/main/java/localhost/milestonenet/MainMenu.java
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -14,5 +18,39 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnavigationview);
+        replaceFragment(new MainDisplayFragment());
+
+//        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+//            Fragment selectedFragment = null;
+//            switch (item.getItemId()) {
+//                case R.id.manage:
+//                    selectedFragment = new ManageFragment();
+//                    break;
+//                case R.id.purchases:
+//                    selectedFragment = new PurchasesFragment();
+//                    break;
+//                case R.id.home:
+//                    selectedFragment = new MainDisplayFragment();
+//                    break;
+//                case R.id.social:
+//                    selectedFragment = new SocialFragment();
+//                    break;
+////                case R.id.logout:
+//                    //logout
+//            }
+//            if (selectedFragment != null) {
+//                replaceFragment(selectedFragment);
+//            }
+//            return true;
+//        });
+
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView4, fragment)
+                .commit();
     }
 }
